e72f3248a24ab858860413b548b40778e502cb36
Candace <32807102+CandaceDowning@users.noreply.github.com>
2019-03-11 21:14:00 -0500

    Update controller.js

--- server/controller.js
+++ server/controller.js
@@ -1,7 +1,7 @@
 const axios = require("axios")
 let base = "https://api.unsplash.com/search/photos/?query="
 
-let access = "&client_id= ///Key removed
+let access = &client_id= ///Key removed
 let favoritesArray= []
 
 
